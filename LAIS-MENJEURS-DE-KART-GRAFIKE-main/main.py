6f3a89dea07d647a4264d46469c189e65f52f1b6
Paul <156648660+PaulXVIII@users.noreply.github.com>
2024-03-19 07:48:50 +0100

    Add files via upload

--- LAIS-MENJEURS-DE-KART-GRAFIKE-main/main.py
+++ LAIS-MENJEURS-DE-KART-GRAFIKE-main/main.py
@@ -1,29 +1,67 @@
 import pygame
 from game import Game
+from pygame.locals import *
+import os
+
 
 pygame.init()
 
 pygame.display.set_caption("Notre RPG")
+pygame.mixer.music.load("boss battle.mp3")
+pygame.mixer.music.play(-1)
+icon = pygame.image.load("icon.jpg")
+pygame.display.set_icon(icon)
 screen = pygame.display.set_mode((1280, 720))
-background = pygame.image.load("hg.jpg")
+background = pygame.image.load("bg.jpg")
 background = pygame.transform.scale(background, (1280, 720))
 menu = pygame.image.load("boutoniu.png")
+title = pygame.image.load("title.png")
 game = Game()
 x_bouton = (1280 - menu.get_width()) / 2
 y_bouton = (720 - menu.get_height()) / 2
+x_titre = (1280 - title.get_width()) / 2
+y_titre = (720 - title.get_height()) / 2 - 170
 running = True
 
+zone1_rect = pygame.Rect(444, 257, 398, 37)
+zone2_rect = pygame.Rect(444, 306, 398, 37)
+zone3_rect = pygame.Rect(444, 353, 398, 37)
+zone4_rect = pygame.Rect(444, 426, 193, 37)
+zone5_rect = pygame.Rect(649, 426, 193, 37)
+
+
 leave_menu = False
+transparent_surface = pygame.Surface((1280, 720), pygame.SRCALPHA)
+transparent_surface.fill((0, 0, 5, 3)) 
+
+
 while running:
     screen.blit(background, (0, 0))
     screen.blit(menu, (x_bouton, y_bouton))
-    if game.pressed.get(pygame.K_SPACE):
-        leave_menu = True
-
-    if leave_menu:  
-        screen.blit(game.player.image, game.player.rect)
-
-
+    screen.blit(title, (x_titre, y_titre))
+    
+    if zone1_rect.collidepoint(pygame.mouse.get_pos()):
+        # Créer une surface semi-transparente pour la zone de collision
+        transparent_surface = pygame.Surface((zone1_rect.width, zone1_rect.height), pygame.SRCALPHA)
+        transparent_surface.fill((0, 0, 255, 64))  # Remplir avec du bleu semi-transparent
+        screen.blit(transparent_surface, (zone1_rect.left, zone1_rect.top))
+    elif zone2_rect.collidepoint(pygame.mouse.get_pos()):
+        transparent_surface = pygame.Surface((zone2_rect.width, zone2_rect.height), pygame.SRCALPHA)
+        transparent_surface.fill((0, 0, 255, 64))
+        screen.blit(transparent_surface, (zone2_rect.left, zone2_rect.top))
+    elif zone3_rect.collidepoint(pygame.mouse.get_pos()):
+        transparent_surface = pygame.Surface((zone3_rect.width, zone3_rect.height), pygame.SRCALPHA)
+        transparent_surface.fill((0, 0, 255, 64))
+        screen.blit(transparent_surface, (zone3_rect.left, zone3_rect.top))
+    elif zone4_rect.collidepoint(pygame.mouse.get_pos()):
+        transparent_surface = pygame.Surface((zone4_rect.width, zone4_rect.height), pygame.SRCALPHA)
+        transparent_surface.fill((0, 0, 255, 64))
+        screen.blit(transparent_surface, (zone4_rect.left, zone4_rect.top))
+    elif zone5_rect.collidepoint(pygame.mouse.get_pos()):
+        transparent_surface = pygame.Surface((zone5_rect.width, zone5_rect.height), pygame.SRCALPHA)
+        transparent_surface.fill((255, 0, 0, 64))
+        screen.blit(transparent_surface, (zone5_rect.left, zone5_rect.top))
+    
     if game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
         game.player.move_left()
 
@@ -35,13 +73,28 @@ while running:
 
     if game.pressed.get(pygame.K_DOWN) and game.player.rect.y + game.player.rect.height < screen.get_height():
         game.player.move_down()
-
-   
-    pygame.display.flip()
-
+    
+    
+    manual_quit = False
+    
     for event in pygame.event.get():
-        # vérifie l'événement est la fermeture de la fenetre
-        if event.type == pygame.QUIT:
+        if event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:  # Bouton gauche de la souris
+                # Début du dessin de la zone
+                if zone1_rect.collidepoint(event.pos):
+                    print("Clic dans la zone 1!")
+                    leave_menu = True
+                # Vérifier si le clic est dans la zone 2
+                elif zone2_rect.collidepoint(event.pos):
+                    print("Clic dans la zone 2!")
+                elif zone3_rect.collidepoint(event.pos):
+                    print("Clic dans la zone 3!")
+                elif zone4_rect.collidepoint(event.pos):
+                    print("Clic dans la zone 4!")
+                else:
+                    print("Clic dans la zone 5!")
+                    manual_quit = True
+        elif event.type == pygame.QUIT or manual_quit:
             running = False
             pygame.quit()
             # une touche pressée
@@ -50,3 +103,6 @@ while running:
             # une touche relâchée
         elif event.type == pygame.KEYUP:
             game.pressed[event.key] = False
+    if leave_menu:
+        screen.blit(game.player.image, game.player.rect)
+    pygame.display.flip()
